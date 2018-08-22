64379ec6f3edf34c9c04c6b7bfefaa17262d10d4
wugf <18620345490@163.com>
2018-08-22 23:37:57 +0800

    百度网盘上传文件

--- selenium-test/src/main/java/com/selenium/KeyboardTest.java
+++ selenium-test/src/main/java/com/selenium/KeyboardTest.java
@@ -2,6 +2,7 @@ package com.selenium;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -47,9 +48,7 @@ public class KeyboardTest {
         loginBtn.click();
 
         // 等待元素加载完毕
-        new WebDriverWait(webDriver, 10).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("TANGRAM__PSP_10__footerULoginBtn")));
-
-        WebElement change = webDriver.findElement(By.id("TANGRAM__PSP_10__footerULoginBtn"));
+        WebElement change = new WebDriverWait(webDriver, 10).until(ExpectedConditions.presenceOfElementLocated(By.id("TANGRAM__PSP_10__footerULoginBtn")));
 
         WebElement account = webDriver.findElement(By.id("TANGRAM__PSP_10__userName"));
 
@@ -67,6 +66,68 @@ public class KeyboardTest {
         Thread.sleep(3 * 1000l);
 
         login.submit();
+    }
 
+    /**
+     * 组合键实现复制，粘贴 CTRL+A, CTRL+C, CTRL+V
+     */
+    @Test
+    public void combination() {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("http://www.w3school.com.cn//tiy/loadtext.asp?f=html_inputfields");
+
+        WebElement name = webDriver.findElement(By.cssSelector("body > form > input[type=\"text\"]:nth-child(1)"));
+
+        WebElement firstName = webDriver.findElement(By.cssSelector("body > form > input[type=\"text\"]:nth-child(3)"));
+
+        // 动作链
+        new Actions(webDriver)
+                .sendKeys(name, "123456")
+                .keyDown(Keys.LEFT_CONTROL)
+                .sendKeys("a")
+                .keyUp(Keys.LEFT_CONTROL)
+                .keyDown(Keys.LEFT_CONTROL)
+                .sendKeys("c")
+                .keyUp(Keys.LEFT_CONTROL)
+                .keyDown(firstName, Keys.LEFT_CONTROL)
+                .sendKeys("v")
+                .keyUp(Keys.LEFT_CONTROL)
+                .perform();
+
+    }
+
+    /**
+     * 实现文件上传
+     */
+    @Test
+    public void upload() throws InterruptedException {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://pan.baidu.com/");
+
+        WebElement change = new WebDriverWait(webDriver, 10).until(ExpectedConditions.presenceOfElementLocated(By.id("TANGRAM__PSP_4__footerULoginBtn")));
+
+        WebElement account = webDriver.findElement(By.id("TANGRAM__PSP_4__userName"));
+        WebElement passwd = webDriver.findElement(By.id("TANGRAM__PSP_4__password"));
+        WebElement login = webDriver.findElement(By.id("TANGRAM__PSP_4__submit"));
+
+        change.click();
+        Thread.sleep(3 * 1000l);
+
+        account.sendKeys("13229942600");
+        Thread.sleep(3 * 1000l);
+
+        passwd.sendKeys("");
+        Thread.sleep(3 * 1000l);
+
+        login.submit();
+
+        // 关闭弹窗
+        new WebDriverWait(webDriver, 10).until(
+                ExpectedConditions.presenceOfElementLocated(By.cssSelector("body > div.module-cardtip-box > div.module-content > div.words > p.tip-button"))).click();
+
+        // 上传操作
+        // 上传按钮
+        WebElement uploadBtn = webDriver.findElement(By.id("h5Input0"));
+        new Actions(webDriver).click(uploadBtn).sendKeys(uploadBtn, "D:\\timg.jpg").perform();
     }
 }
