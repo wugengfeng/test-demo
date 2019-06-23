a4384aac8675655c64ad6ecb34a0104d97b09dfa
744526152@qq.com <qq3410650>
2019-06-24 01:34:24 +0800

    新增项目提交

---
+++ springcloud-seata/storage-server/src/main/java/com/wgf/storage/StorageServerApplication.java
@@ -0,0 +1,22 @@
+package com.wgf.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+// 启动eureka
+@EnableEurekaClient
+// 启动feign
+@EnableFeignClients
+// 启动熔断器
+@EnableCircuitBreaker
+@SpringBootApplication
+public class StorageServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageServerApplication.class, args);
+	}
+
+}
