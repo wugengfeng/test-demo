095b5df5477182c6f365a986372a926c1f5015b8
wugengfeng <z515535@2980.cpm>
2019-07-20 20:18:07 +0800

    爬虫提交

---
+++ auth-code/src/main/java/com/guarantee/authcode/AuthCodeApplication.java
@@ -0,0 +1,13 @@
+package com.guarantee.authcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthCodeApplication.class, args);
+    }
+
+}
